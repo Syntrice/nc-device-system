776d11103d4157e9e7b4a2f40d3fa4c584aa2ef4
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-12 15:49:38 +0000

    Create abstract class Device

---
+++ Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_abstraction
+{
+    internal abstract class Device
+    {
+        public string Name { get; set; }
+
+        public bool Connected { get; set; }
+
+        public Device(string name)
+        {
+            Name = name;
+            Connected = false;
+        }
+    }
+}
