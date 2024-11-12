15408af7c8c1344979d2d4d90c1cc5aefaed419d
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-12 16:36:27 +0000

    Add new IRunnable implementation Operation

---
+++ Operation.cs
@@ -0,0 +1,23 @@
+﻿namespace NcDeviceSystem
+{
+    internal class Operation : IRunnable
+    {
+        public string Name { get; set; }
+
+        public bool RequiresConnection { get; set; }
+
+        private readonly string operation;
+
+        public Operation(string name, bool requiresConnection, string operation)
+        {
+            Name = name;
+            RequiresConnection = requiresConnection;
+            this.operation = operation;
+        }
+
+        public void Run(bool connected)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
